1843d7011ff95bc5cc4442ba6cdb2b36033eafab
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-09 14:39:09 +0900

    Updated register.jsx to get variables

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Debug 
+    console.log(
+      `Form submitted with values ${username}, ${password}, ${password2}`
+    );
+  };
+
   return (
     <>
       <h1>Register</h1>
-      <form action="" method="POST">
-        <input type="text" placeholder="Username" name="username"></input>
-        <input type="password" placeholder="Password" name="password"></input>
-        <input type="password" placeholder="Confirm password" name="password-2"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Confirm password"
+          value={password2}
+          onChange={(event) => setPassword2(event.target.value)}
+        ></input>
         <input type="submit" value="Register"></input>
       </form>
     </>
