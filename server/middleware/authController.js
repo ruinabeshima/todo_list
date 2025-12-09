e1e4eb462425c3d16b00273f93fc97bff2cc80b1
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-09 16:45:29 +0900

    Basic validation in backend

--- server/middleware/authController.js
+++ server/middleware/authController.js
@@ -1,6 +1,28 @@
 async function RegisterUser(req, res) {
-  const { username, password, password2 } = req.body;
-  res.json({ username, password, password2 })
+  try {
+    const { username, password, password2 } = req.body;
+
+    // No inputs
+    if (!username || !password || !password2) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    // Passwords do not match
+    if (password !== password2) {
+      return res.status(409).json({ message: "Passwords do not match" });
+    }
+
+    // Password should be at least 8 characters
+    if (password.length < 8) {
+      return res.status(400).json({ message: "Password is too short" });
+    }
+
+    // Username already exists
+    // SQL Query
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 module.exports = { RegisterUser };
