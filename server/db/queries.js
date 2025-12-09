199d0343868eb4766c47ece93ddb72c622aa7319
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-09 21:40:54 +0900

    Finished account validation with SQL queries

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,4 +1,4 @@
-const { pool } = require("./dbconfig.js");
+const pool = require("./dbconfig.js");
 
 async function getUser(username) {
   const user = await pool.query("SELECT * FROM users WHERE username = $1", [
@@ -8,10 +8,11 @@ async function getUser(username) {
 }
 
 async function insertUser(username, password) {
-  await pool.query(
+  const result = await pool.query(
     "INSERT INTO users (username, password) VALUES($1, $2) RETURNING id, username",
     [username, password]
   );
+  return result.rows[0];
 }
 
 module.exports = { getUser, insertUser };
--- server/middleware/authController.js
+++ server/middleware/authController.js
@@ -1,3 +1,5 @@
+const db = require("../db/queries.js");
+
 async function RegisterUser(req, res) {
   try {
     const { username, password, password2 } = req.body;
@@ -5,6 +7,12 @@ async function RegisterUser(req, res) {
     // No inputs
     if (!username || !password || !password2) {
       return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    // Username already exists
+    const user = await db.getUser(username);
+    if (user) {
+      return res.status(409).json({ message: "Username already exists" });
     }
 
     // Passwords do not match
@@ -17,8 +25,6 @@ async function RegisterUser(req, res) {
       return res.status(400).json({ message: "Password is too short" });
     }
 
-    // Username already exists
-    // SQL Query
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
