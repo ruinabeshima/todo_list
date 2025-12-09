2b03c29cdca95e2524a5dbed2702a6d51fdfe206
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-09 16:04:56 +0900

    Handling data through React

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function Register() {
+  const [errorMessage, setErrorMessage] = useState("")
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
@@ -8,10 +9,40 @@ export default function Register() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Debug 
-    console.log(
-      `Form submitted with values ${username}, ${password}, ${password2}`
-    );
+    async function handleData() {
+      // Debug
+      console.log(
+        `Form submitted with values ${username}, ${password}, ${password2}`
+      );
+
+      try {
+        const response = await fetch("http://localhost:8080/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+            password2: password2,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`Reponse status = ${response.status}`);
+        }
+
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        setErrorMessage(error.message)
+
+        //Debug
+        console.log(error.message);
+      }
+    }
+
+    handleData();
   };
 
   return (
@@ -37,6 +68,10 @@ export default function Register() {
           onChange={(event) => setPassword2(event.target.value)}
         ></input>
         <input type="submit" value="Register"></input>
+
+        {errorMessage && (
+          <p>{errorMessage}</p>
+        )}
       </form>
     </>
   );
