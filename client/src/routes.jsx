e33b981ed47d8c093126471edc64d73d62ffe6c8
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-10 23:52:42 +0900

    Set up dashboard page and route

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/register";
 import Login from "./components/login";
+import Dashboard from "./components/dashboard";
 
 const routes = (
   <Routes>
     <Route path="/register" element={<Register />}></Route>
     <Route path="/login" element={<Login />}></Route>
+    <Route path="/dashboard" element={<Dashboard />}></Route>
   </Routes>
 )
 
